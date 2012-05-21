4f0217a0e2de423628874ba98a9b825d0998e3ae
Dan Hayes <dhayes@capmarktech.com>
2012-05-21 17:57:02 -0400

    Added setup_env.py for access to django trading models

---
+++ setup_env.py
@@ -0,0 +1,6 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cashflow.settings'
+
+import sys
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "/home/ubuntu/django_projects/cashflow/"))
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "/home/ubuntu/django_projects/"))
